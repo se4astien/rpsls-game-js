5a31496a5226fa8d8ca1ad19633efa8a1ef85ce8
sebv <sebv@sv.home>
2020-04-24 12:14:37 +0200

    first version of rock paper scissors

---
+++ js/main.js
@@ -0,0 +1,143 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+
+const modal = document.querySelector('.modal');
+const content = document.getElementById('content');
+
+const scoreboard = {
+  player: 0,
+};
+
+// 1. When you click on a picture (rock, paper or scissors)
+function play(e) {
+  // choice of player
+  const playerChoice = e.target.id;
+  // choice of computer
+  const computerChoice = getComputerChoice();
+  console.log(playerChoice, computerChoice);
+  // choose a winner between playerChoice and computerChoice
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, playerChoice, computerChoice);
+}
+
+// 2. Select rock / paper or scissors
+function getComputerChoice() {
+  const random = Math.random();
+  if (random < 0.34) {
+    return 'rock';
+  } else if (random < 0.68) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+// 3. Show winner
+function getWinner(p, c) {
+  if (p === c) {
+    return 'draw';
+  } else if (p === 'rock') {
+    if (c === 'paper') {
+      return 'computer';
+    } else {
+      return 'player';
+    }
+  } else if (p === 'paper') {
+    if (c === 'scissors') {
+      return 'computer';
+    } else {
+      return 'player';
+    }
+  } else if (p === 'scissors') {
+    if (c === 'rock') {
+      return 'computer';
+    } else {
+      return 'player';
+    }
+  }
+}
+
+// 5. Show winner
+function showWinner(winner, playerChoice, computerChoice) {
+  if (winner === 'player') {
+    scoreboard.player++;
+    result.innerHTML = `
+    <div>
+        <h3>You picked</h3>
+        <span class=${playerChoice}>
+            <img src="../images/icon-${playerChoice}.svg" alt=${playerChoice} />
+          </span>
+    </div>
+    <div>
+        <h2 class="text-win">You win</h2>
+        <button id="again" class="again-btn">Play Again</button>
+    </div>
+    <div>
+        <p>The house picked</p>
+        <span class=${computerChoice}>
+            <img src="./images/icon-${computerChoice}.svg" alt=${computerChoice} />
+        </span>
+    </div>
+    `;
+  } else if (winner === 'computer') {
+    result.innerHTML = `
+      <div>
+      <p>You picked</p>
+      <span class=${playerChoice}>
+        <img src="./images/icon-${playerChoice}.svg" alt=${playerChoice} />
+      </span>
+    </div>
+    <div>
+      <h2 class="text-lose">You lose</h2>
+      <button id="again" class="again-btn">Play Again</button>
+    </div>
+    <div>
+      <p>The house picked</p>
+      <span class=${computerChoice}>
+        <img src="./images/icon-${computerChoice}.svg" alt=${computerChoice} />
+      </span>
+    </div>
+      `;
+  } else {
+    result.innerHTML = `
+      <div>
+      <p>You picked</p>
+      <span class=${playerChoice}>
+        <img src="./images/icon-${playerChoice}.svg" alt=${playerChoice} />
+      </span>
+    </div>
+    <div>
+      <h2>It's a draw</h2>
+      <button id="again" class="again-btn">Play Again</button>
+    </div>
+    <div>
+      <p>The house picked</p>
+      <span class=${computerChoice}>
+        <img src="./images/icon-${computerChoice}.svg" alt=${computerChoice} />
+      </span>
+    </div>
+      `;
+  }
+
+  // Show score
+  score.innerHTML = `
+    <span>Score</span>
+    <span>${scoreboard.player}</span>
+  `;
+
+  // Show modal
+  content.style.display = 'none';
+  modal.style.display = 'block';
+}
+
+// 4. Play Again
+function playAgain(e) {
+  if (e.target == again) {
+    modal.style.display = 'none';
+    content.style.display = 'block';
+  }
+}
+
+choices.forEach((choice) => choice.addEventListener('click', play));
+result.addEventListener('click', playAgain);
